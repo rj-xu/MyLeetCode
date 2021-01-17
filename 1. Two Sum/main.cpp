8fd574b62025a573da3f4a3e6a5814dd4d27bc9d
xuxiabb <xuxiabb@hotmail.com>
2021-01-18 00:23:41 +0800

    📦 NEW: Add 1. Two Sum

---
+++ 1. Two Sum/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> m;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            auto iter = m.find(target - nums[i]);
+            if (iter != m.end())
+                return {iter->second, i};
+            else
+                m[nums[i]] = i;
+        }
+        return {};
+    }
+};
--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 [TOC]
 
+## 1. Two Sum
+
+1. It is feasible to use enumeration. **Time complexity O(n)**.
+2. We can convert the enumeration to a hash map. **Time complexity O(1)**
+
 ## 1232. Check If It Is a Straight Line
 
 1. This is a linear equation of two variables, we need to traverse all points.
