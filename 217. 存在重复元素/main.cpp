a3ab67e24da2332a000c7c01f23dd028e921eee9
RJ Xu <Rongji.Xu@outlook.com>
2022-05-08 22:31:42 +0800

    📦 NEW:

--- 217. 存在重复元素/main.cpp
+++ 217. 存在重复元素/main.cpp
@@ -1,7 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution
 {
 public:
     bool containsDuplicate(vector<int> &nums)
     {
+        auto n = nums.size();
+        unordered_set<int> s;
+        for (auto x : nums)
+        {
+            if (s.find(x) != s.end())
+            {
+                return true;
+            }
+            s.insert(x);
+        }
+        return false;
     }
 };
