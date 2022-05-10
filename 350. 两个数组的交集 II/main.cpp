9c7d2a1b329ac91f356b8e82629f4d81673b66f4
RJ Xu <Rongji.Xu@outlook.com>
2022-05-10 21:59:11 +0800

    📦 NEW:

--- 350. 两个数组的交集 II/main.cpp
+++ 350. 两个数组的交集 II/main.cpp
@@ -34,5 +34,42 @@ public:
     }
 };
 
-
 // 排序+双指针
+// 时间复杂度：O(mlogm + nlogn)
+// 空间复杂度：O(min(m,n))
+class Solutions
+{a
+public:
+    vector<int> intersect(vector<int> &nums1, vector<int> &nums2)
+    {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        auto len1 = nums1.size();
+        auto len2 = nums2.size();
+
+        int p1 = 0;
+        int p2 = 0;
+        vector<int> rc;
+
+        while (p1 < len1 && p2 < len2)
+        {
+            if (nums1[p1] < nums2[p2])
+            {
+                p1++;
+            }
+            else if (nums1[p1] > nums2[p2])
+            {
+                p2++;
+            }
+            else
+            {
+                rc.push_back(nums1[p1]);
+                p1++;
+                p2++;
+            }
+        }
+
+        return rc;
+    }
+};
