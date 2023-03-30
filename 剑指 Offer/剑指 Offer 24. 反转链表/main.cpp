8cedd2b295b75d51aa4b6906ae34d962481d2abe
RJ Xu <Rongji.Xu@outlook.com>
2023-03-30 11:18:35 +0800

    New

---
+++ 剑指 Offer/剑指 Offer 24. 反转链表/main.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* node = head;
+        ListNode* pre = NULL;
+        while(node)
+        {
+            ListNode* temp = node->next;
+            node->next = pre;
+            pre = node;
+            node = temp;
+        }
+        return pre;
+    }
+};
