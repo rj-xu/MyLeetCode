9adb21c316222c8b359395acb510cd412b2a79e0
RJ Xu <Rongji.Xu@outlook.com>
2023-04-23 17:37:52 +0800

    new

---
+++ 剑指 Offer/剑指 Offer 53 - I. 在排序数组中查找数字 I/main.cpp
@@ -0,0 +1,44 @@
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int l = 0;
+        int r = nums.size() - 1;
+        int m = 0;
+
+        while (l <= r)
+        {
+            m = (l + r) / 2;
+            if (nums[m] < target)
+            {
+                l = m + 1;
+            }
+            else
+            {
+                r = m - 1;
+            }
+        }
+
+        int rc_l = l;
+
+        l = m;
+        r = nums.size() - 1;
+        while (l <= r)
+        {
+            int m = (l + r) / 2;
+            if (nums[m] <= target)
+            {
+                l = m + 1;
+            }
+            else
+            {
+                r = m - 1;
+            }
+        }
+
+        int rc_r = r;
+
+        return (rc_r - rc_l + 1);
+    }
+};
